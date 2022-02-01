8712aaf0ad70d5a63f074adbc1d750d447a221aa
Lev <levparygin3@gmail.com>
2022-02-01 19:23:50 +0600

    Purrweb task
    Purrweb task

---
+++ site purrweb/libs/main.js
@@ -0,0 +1,10 @@
+$(function (){
+    $('.slider').slick({
+        dots:true,
+        adaptiveHeight:true,
+        slidesToShow:2,
+        speed:1337,
+        autoplay:true,
+        autoplaySpeed:2000
+    });
+});
